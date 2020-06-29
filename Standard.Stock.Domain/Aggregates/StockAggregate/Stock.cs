f9f67e7a81227bea167185d9f02c80824313be45
LAPTOP-6MJO2FAV\igorm <igor.meier@gmail.com>
2020-06-29 08:39:00 -0300

    Desenvolvimento da api de stocks

---
+++ Standard.Stock.Domain/Aggregates/StockAggregate/Stock.cs
@@ -0,0 +1,34 @@
+﻿using Standard.Framework.Seedworks.Domains.Abstraction;
+using System;
+
+namespace Standard.Stock.Domain.StockAggregate
+{
+    public class Stock : Entity, IAggregateRoot
+    {
+        public Guid StockId { get; private set; }
+        public string Name { get; private set; }
+        public string Initials { get; private set; }
+        public string Company { get; private set; }
+        public DateTime LaunchDate { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+        public bool Enabled { get; private set; }
+
+        public Stock(string name,
+                     string initials,
+                     string company,
+                     DateTime launchDate,
+                     double price,
+                     int quantity,
+                     bool enabled = true) 
+        {
+            Name = name;
+            Initials = initials;
+            Company = company;
+            LaunchDate = launchDate;
+            Price = price;
+            Quantity = quantity;
+            Enabled = enabled;
+        }
+    }
+}
---
+++ Standard.Stock.Domain/Aggregates/TransactionAggregate/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using Standard.Framework.Seedworks.Domains.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Standard.Stock.Domain.Aggregates.TransactionAggregate
+{
+    public class Transaction : Entity, IAggregateRoot
+    {
+
+    }
+}
