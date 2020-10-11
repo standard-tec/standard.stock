a3b6f5e3b4424f55919948b306be382017a18f4f
Igor Tavares Baarata <igor.meier@gmail.com>
2020-10-11 10:45:13 -0300

    Ajustes da configuração para utilização de secrets

--- Standard.Stock.WebApi/Startup.cs
+++ Standard.Stock.WebApi/Startup.cs
@@ -20,8 +20,9 @@ namespace Standard.Stock
         {
             Builder = new ConfigurationBuilder().SetBasePath(Path.Combine(env.ContentRootPath, "Settings"))
                                                 .AddJsonFile($"connectionstrings.{env.EnvironmentName}.json", true, true)
-                                                .AddJsonFile($"appsettings.json", true, true)
                                                 .AddJsonFile($"messagebroker.{env.EnvironmentName}.json", true, true)
+                                                .AddJsonFile($"secrets/connectionstrings.{env.EnvironmentName}.json", true, true)
+                                                .AddJsonFile($"secrets/messagebroker.{env.EnvironmentName}.json", true, true)
                                                 .AddEnvironmentVariables();
 
             Configuration = Builder.Build();
